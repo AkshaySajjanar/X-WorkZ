9df8d623945e5b9729989bb15b7bd1ddc7e88b32
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-09-02 21:04:44 +0530

    Create MostUsed.java

---
+++ MostUsed.java
@@ -0,0 +1,10 @@
+class MostUsed{
+		public static void main(String args[]){
+		
+			System.out.println("Mobile ");
+			System.out.println("Brush ");
+			System.out.println("TV ");
+			System.out.println("Food ");
+			System.out.println("Laptop");
+		}
+}

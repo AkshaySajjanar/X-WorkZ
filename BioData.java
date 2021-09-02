c0ac665c53d45d746dd20f086d1eec5f010fc5ce
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-09-02 21:03:50 +0530

    Create BioData.java

---
+++ BioData.java
@@ -0,0 +1,9 @@
+class BioData{
+		public static void main(String args[]){
+		
+			System.out.println("Name: Akshay Sajjanar ");
+			System.out.println("College: BGMIT Mudhol ");
+			System.out.println("Branch: ECE ");
+			System.out.println("YOP: 2020");
+		}
+}

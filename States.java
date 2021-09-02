dbf1664da0a37ba89f231fa0e3d7f58d829c575a
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-09-02 21:02:33 +0530

    Create States.java

---
+++ States.java
@@ -0,0 +1,11 @@
+class States{
+		public static void main(String args[]){
+			System.out.println("5 states in India are:");
+			System.out.println("Karnataka ");
+			System.out.println("Tamil Nadu ");
+			System.out.println("Andhra Pradesh ");
+			System.out.println("Telangana ");
+			System.out.println("Kerala ");
+		
+		}
+}

535d3c781da845736e44ebcdff96896db0a29cb5
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-09-17 06:52:31 +0530

    methods

--- methods/EvenNumbers.java
+++ methods/EvenNumbers.java
@@ -2,21 +2,18 @@ package com.xworkz.methods;
 
 public class EvenNumbers {
 
-	public static int[] arr() {
-		int numbers[] = { 112, 111, 116, 117, 120, 121, 124, 129, 128, 130 };
-
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] % 2 == 0) {
-				System.out.println(numbers[i]);
+	public static void arrayElements(int arrayEvenElements[]) {
+		for (int i = 0; i < arrayEvenElements.length; i++) {
+			if (arrayEvenElements[i] % 2 == 0) {
+				System.out.println("Even numberd are " + arrayEvenElements[i]);
 			}
 		}
-		return numbers;
 	}
 
 	public static void main(String[] args) {
 
-		int a[] = EvenNumbers.arr();
-		System.out.println(a);
+		int arrayEvenNumbers[] = { 2, 1, 24, 34, 45, 55, 66, 18, 11, 10 };
+		EvenNumbers.arrayElements(arrayEvenNumbers);
 	}
 
 }

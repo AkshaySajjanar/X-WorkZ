01e678d0dfbd46e778282a1f29ac89c25d9752c9
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-10-18 23:06:30 +0530

    String

--- string/Email.java
+++ string/Email.java
@@ -1,14 +1,23 @@
 package com.xworkz.string;
 
 public class Email {
-	String mail = "akshay.sajjanar@gmail.com";
 
-	public String mailVerify() {
-
-		if (mail.contains("@") && mail.endsWith(".com")) {
-			return "Email ID is valid";
+	public boolean mail() {
+		String mailId = "akshay.sajjanar@gmail.com";
+		boolean flag = true;
+		if (mailId.isEmpty()) {
+			System.out.println("Please Enter a Valid Email ID");
+			return false;
+		} else if (mailId.length() < 11) {
+			System.out.println("Email should contain more than 10 characters ");
+			return false;
+		} else if (!(mailId.endsWith("@gmail.com"))) {
+			System.out.println("Enter valid provider name");
+			return false;
+		} else {
+			System.out.println("Email ID is valid");
 		}
-		return "Invalid Email ID";
-	}
 
+		return flag;
+	}
 }
--- string/EmailTest.java
+++ string/EmailTest.java
@@ -5,8 +5,8 @@ public class EmailTest {
 	public static void main(String[] args) {
 
 		Email email = new Email();
-		String mailVerify = email.mailVerify();
-		System.out.println(mailVerify);
+		email.mail();
+		
 	}
 
 }

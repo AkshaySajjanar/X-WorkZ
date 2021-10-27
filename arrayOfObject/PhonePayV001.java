f0bce169f6a319ebdef888c1ba4ca38a5c9aa551
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-10-27 07:43:20 +0530

    Array of objects

--- arrayOfObject/PhonePayV001.java
+++ arrayOfObject/PhonePayV001.java
@@ -6,9 +6,20 @@ public class PhonePayV001 {
 	int releaseYear;
 
 	public PhonePayV001(String name, String version, int releaseYear) {
+		this(name);
+		System.out.println("Parameter constucotr");
 		this.name = name;
 		this.version = version;
 		this.releaseYear = releaseYear;
+	}
+
+	public PhonePayV001() {
+		System.out.println("NO parameter Constructor");
+	}
+
+	public PhonePayV001(String name) {
+		this();
+		System.out.println("One parameter constrctor");
 	}
 
 	public void login() {

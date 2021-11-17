8f9fcaa9db10fdd29c34b609d619dc955e09fbe5
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-11-17 16:10:00 +0530

    Interfaces

--- interface/DB/Dao/DBOperationDao.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.dataTransfer_dataAccess.DB.Dao;
-
-public interface DBOperationDao {
-
-	public void connectDb(String user, String password);
-	public void save(String name, String place);
-	public void delete(String name);
-	public void update(String name, String place);
-}

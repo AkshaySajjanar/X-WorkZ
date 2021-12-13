37f0e3e7bd35c9b075f332356b25b11bef6ac29b
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-12-13 13:44:05 +0530

    Logic for counting the number of words without the inbuilt function

--- HomeWork/CountWords.java
+++ HomeWork/CountWords.java
@@ -3,14 +3,20 @@ package com.xworkz.HomeWork;
 public class CountWords {
 
 	public static void main(String[] args) {
-		
+
 		String a = "xworkz is located in Rajajinagar, Bangalore. It is sorftware training institution.";
-		
-		String[] count = a.split(" ");
-		
-		int a1 = count.length;
-		
-		System.out.println(a1);
-		
+		char[] charArray = a.toCharArray();
+		int count = 1;
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] == ' ') {
+				count++;
+			}
+		}
+		System.out.println(count);
+
+		/*
+		 * String[] count = a.split(" "); int a1 = count.length; System.out.println(a1);
+		 */
+
 	}
 }

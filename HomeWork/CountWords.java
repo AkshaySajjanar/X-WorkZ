6b2d5091eedf99efb3a1d8b62dd84cfc297d8861
AkshaySajjanar <89777919+AkshaySajjanar@users.noreply.github.com>
2021-12-13 13:25:22 +0530

    Counting the number of words

---
+++ HomeWork/CountWords.java
@@ -0,0 +1,16 @@
+package com.xworkz.HomeWork;
+
+public class CountWords {
+
+	public static void main(String[] args) {
+		
+		String a = "xworkz is located in Rajajinagar, Bangalore. It is sorftware training institution.";
+		
+		String[] count = a.split(" ");
+		
+		int a1 = count.length;
+		
+		System.out.println(a1);
+		
+	}
+}
